dfc9913ad7692df2ece0a2b85957619af75c49be
ZZB <realbo89@outlook.com>
2015-08-31 11:47:55 +0800

    BaseActivity

---
+++ app/src/main/java/com/zzb/weibo/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.zzb.weibo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZZB on 2015/8/31.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
