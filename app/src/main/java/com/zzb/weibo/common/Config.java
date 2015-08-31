5d4ee174a6b0f036ca8193945f5b024e9cb5e87c
ZZB <realbo89@outlook.com>
2015-08-31 18:07:14 +0800

    Config

--- app/src/main/java/com/zzb/weibo/common/Config.java
+++ app/src/main/java/com/zzb/weibo/common/Config.java
@@ -7,8 +7,8 @@ import com.zzb.weibo.BuildConfig;
  */
 public class Config {
 //    // 请上 open.weibo.com 自行注册应用并替换下面两个参数
-//    public static final String APP_SECRET = LocalConfig.APP_SECRET;
-//    public static final String APP_KEY = LocalConfig.APP_KEY;
+    public static final String APP_SECRET = BuildConfig.WEIBO_APP_SECRET;
+    public static final String APP_KEY = BuildConfig.WEIBO_APP_KEY;
 //    /**Bugly监控, bugly代码可以注释*/
     public static final String BUGLY_APP_ID = BuildConfig.BUGLY_APP_ID;
     public static final String BUGLY_APP_KEY = BuildConfig.BUGLY_APP_KEY;
