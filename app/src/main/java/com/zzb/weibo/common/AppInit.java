b4b2d6d4344ba45efc253a548f1145ab79957d58
ZZB <realbo89@outlook.com>
2015-08-31 17:47:34 +0800

    添加bugly

---
+++ app/src/main/java/com/zzb/weibo/common/AppInit.java
@@ -0,0 +1,55 @@
+package com.zzb.weibo.common;
+
+import com.tencent.bugly.crashreport.CrashReport;
+import com.zzb.weibo.BuildConfig;
+import com.zzb.weibo.MyApplication;
+
+/**
+ * Created by ZZB on 2015/8/31.
+ */
+public class AppInit {
+    private static AppInit instance;
+
+    public static AppInit getInstance(){
+        if(instance == null){
+            instance = new AppInit();
+        }
+        return instance;
+    }
+    private AppInit(){
+    }
+
+    public void run() {
+        common();
+        if(BuildConfig.DEBUG){
+            debug();
+        }else{
+            product();
+        }
+    }
+    private void common(){
+        initBugly();
+    }
+    /**
+     * 调试环境配置
+     *created at 2015/8/31 17:35
+     */
+    private void debug(){
+
+    }
+    /**
+     * 正式环境配置
+     *created at 2015/8/31 17:34
+     */
+    private void product(){
+
+    }
+
+    /**
+     * 异常监控bugly初始化
+     *created at 2015/8/31 17:37
+     */
+    private void initBugly(){
+        CrashReport.initCrashReport(MyApplication.APP_CONTEXT, BuildConfig.BUGLY_APP_ID, BuildConfig.DEBUG);
+    }
+}
