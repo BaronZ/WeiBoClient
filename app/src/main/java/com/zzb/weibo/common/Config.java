81cd5ae48e8f98a3e31e55afbde2a4496b7006f7
ZZB <realbo89@outlook.com>
2015-08-31 14:32:02 +0800

    Config

---
+++ app/src/main/java/com/zzb/weibo/common/Config.java
@@ -0,0 +1,10 @@
+package com.zzb.weibo.common;
+
+/**
+ * Created by ZZB on 2015/8/31.
+ */
+public class Config {
+    // 请上 open.weibo.com 自行注册应用并替换下面两个参数
+    public static final String APP_SECRET = LocalConfig.APP_SECRET;
+    public static final String APP_KEY = LocalConfig.APP_KEY;
+}
