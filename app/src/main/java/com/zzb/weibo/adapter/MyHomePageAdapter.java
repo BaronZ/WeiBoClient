4cd859e37741fce09b196c0637e5370e9dd1abe6
BaronZ <realbo89@outlook.com>
2015-09-15 00:06:23 +0800

    大坑原来是RecyclerView里面的Recycler没有设置LayoutManager，耗了我几个小时

--- app/src/main/java/com/zzb/weibo/adapter/MyHomePageAdapter.java
+++ app/src/main/java/com/zzb/weibo/adapter/MyHomePageAdapter.java
@@ -1,6 +1,9 @@
 package com.zzb.weibo.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +20,8 @@ import java.util.List;
  * 我的首页相关
  * Created by ZZB on 2015/9/8.
  */
-public class MyHomePageAdapter extends RecyclerView.Adapter {
-
+public class MyHomePageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String TAG = MyHomePageAdapter.class.getSimpleName();
     private List<Status> mData;
 
     public void setData(List<Status> data) {
@@ -27,25 +30,37 @@ public class MyHomePageAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        RecyclerView.ViewHolder holder = null;
+        Context context = parent.getContext();
+        NormalViewHolder holder = null;
         View itemView = null;
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        LayoutInflater inflater = LayoutInflater.from(context);
         switch (viewType) {
             case ViewType.NORMAL_TEXT_WEIBO:
+                Log.d(TAG, "onCreateViewHolder NORMAL_TEXT_WEIBO" );
                 itemView = inflater.inflate(R.layout.rv_normal_text_weibo, parent, false);
                 holder = new NormalViewHolder(itemView);
                 break;
             case ViewType.NORMAL_PICS_WEIBO:
+                Log.d(TAG, "onCreateViewHolder NORMAL_PICS_WEIBO" );
                 itemView = inflater.inflate(R.layout.rv_normal_pics_weibo, parent, false);
                 holder = new NormalViewHolder(itemView);
+                if(holder.mRvPics.getLayoutManager() == null){
+                    holder.mRvPics.setLayoutManager(new LinearLayoutManager(context));
+                }
                 break;
             case ViewType.FORWARD_TEXT_WEIBO:
+                Log.d(TAG, "onCreateViewHolder FORWARD_TEXT_WEIBO" );
                 itemView = inflater.inflate(R.layout.rv_repost_text_weibo, parent, false);
                 holder = new ForwardViewHolder(itemView);
+                holder.mRvPics.setVisibility(View.GONE);
                 break;
             case ViewType.FORWARD_PICS_WEIBO:
+                Log.d(TAG, "onCreateViewHolder FORWARD_PICS_WEIBO" );
                 itemView = inflater.inflate(R.layout.rv_repost_pics_weibo, parent, false);
                 holder = new ForwardViewHolder(itemView);
+                if(holder.mRvPics.getLayoutManager() == null){
+                    holder.mRvPics.setLayoutManager(new LinearLayoutManager(context));
+                }
                 break;
         }
         return holder;
@@ -74,7 +89,6 @@ public class MyHomePageAdapter extends RecyclerView.Adapter {
 
     private void populateBaseViewHolder(int position, NormalViewHolder baseViewHolder) {
         Status status = mData.get(position);
-
         baseViewHolder.mTvUserName.setText(status.user.name);
         baseViewHolder.mTvTime.setText(status.createdAt);
 //        baseViewHolder.mTvStatus.setText();
@@ -87,13 +101,14 @@ public class MyHomePageAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemViewType(int position) {
-//        Status status = mData.get(position);
-//        if(status.retweetedStatus != null){
-//            return status.retweetedStatus.originalPic == null ? ViewType.NORMAL_TEXT_WEIBO : ViewType.NORMAL_PICS_WEIBO;
-//        }else{
-//            return status.originalPic == null ? ViewType.NORMAL_TEXT_WEIBO : ViewType.NORMAL_PICS_WEIBO;
-//        }
-        return ViewType.NORMAL_PICS_WEIBO;
+        Status status = mData.get(position);
+        int viewType = -1;
+        if(status.retweetedStatus != null){
+            viewType = status.retweetedStatus.originalPic == null ? ViewType.NORMAL_TEXT_WEIBO : ViewType.NORMAL_PICS_WEIBO;
+        }else{
+            viewType = status.originalPic == null ? ViewType.NORMAL_TEXT_WEIBO : ViewType.NORMAL_PICS_WEIBO;
+        }
+        return viewType;
     }
 
     private static class NormalViewHolder extends RecyclerView.ViewHolder{
@@ -109,6 +124,7 @@ public class MyHomePageAdapter extends RecyclerView.Adapter {
             mTvTime = $(R.id.tv_time);
             mTvFrom = $(R.id.tv_from);
             mTvStatus = $(R.id.tv_status);
+            mRvPics = $(R.id.rv_pics);
         }
         TextView mTvUserName, mTvTime, mTvFrom, mTvStatus, mTvComment, mTvForward, mTvGood;
         ImageView mIvIcon;
